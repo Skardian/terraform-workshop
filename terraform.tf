3cc8df225129f5c820f3c7669f002e8b57ce781e
Pepe Padial <jose.carlos.padial@gmail.com>
2017-11-01 17:44:59 +0100

    Create a custom security group

--- terraform.tf
+++ terraform.tf
@@ -5,4 +5,18 @@ provider "aws" {
 resource "aws_instance" "my_instance" {
   ami           = "ami-acd005d5" # Amazon Linux AMI 2017.09
   instance_type = "t2.micro"
+
+  security_groups = ["${aws_security_group.my_security_group.name}"]
+}
+
+resource "aws_security_group" "my_security_group" {
+  name        = "terraform-workshop-sg"
+  description = "Terraform workshop security group"
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 }
